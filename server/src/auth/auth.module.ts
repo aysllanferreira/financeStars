4925a2c7f6acd437d6c662cc845c4f7e91f7ec98
aysllanferreira <aysllan.ferreira@pinely.eu>
2023-06-10 18:17:03 -0300

    :sparkles: feat: Service created

--- server/src/auth/auth.module.ts
+++ server/src/auth/auth.module.ts
@@ -1,9 +1,10 @@
 import { Module } from '@nestjs/common';
 import { AwsCognitoStrategy } from './strategies/aws-cognito.strategy';
 import { AuthGuard } from './guards/auth.guard';
+import { AuthService } from './auth.service';
 
 @Module({
-  providers: [AwsCognitoStrategy, AuthGuard],
+  providers: [AwsCognitoStrategy, AuthGuard, AuthService],
   exports: [AuthGuard],
 })
 export class AuthModule {}
