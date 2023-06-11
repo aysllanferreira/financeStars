75be16ee47ccb6f39a28e13dc417e53bbcb2c312
aysllanferreira <aysllan.ferreira@pinely.eu>
2023-06-10 21:09:28 -0300

    :fire: remove: Removing unncessary files

--- server/src/auth/auth.module.ts
+++ server/src/auth/auth.module.ts
@@ -1,12 +1,10 @@
 import { Module } from '@nestjs/common';
 import { AwsCognitoStrategy } from './strategies/aws-cognito.strategy';
-import { AuthGuard } from './guards/auth.guard';
 import { AuthService } from './auth.service';
 import { AuthController } from './auth.controller';
 
 @Module({
-  providers: [AwsCognitoStrategy, AuthGuard, AuthService],
-  exports: [AuthGuard],
+  providers: [AwsCognitoStrategy, AuthService],
   controllers: [AuthController],
 })
 export class AuthModule {}
