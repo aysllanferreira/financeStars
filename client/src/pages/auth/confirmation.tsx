f8bf6070656ead3a4ba44ea80054014855e65a67
aysllanferreira <aysllan.ferreira@pinely.eu>
2023-06-11 20:39:31 -0300

    :sparkles: feat: Code Confirmation page style

---
+++ client/src/pages/auth/confirmation.tsx
@@ -0,0 +1,32 @@
+import { useRouter } from 'next/router';
+
+export default function Confirmation() {
+  const router = useRouter();
+  return (
+    <main className="bg-gray-100 min-h-screen flex items-center justify-center">
+      <div className="bg-white p-8 rounded shadow-md w-[450px]">
+        <h1 className="text-2xl font-bold mb-4">Finance Star</h1>
+        <p className="text-gray-700 mb-6">Sign Up</p>
+
+        <form className="grid gap-4">
+          <label htmlFor="email" className="text-gray-700">
+            Email
+          </label>
+          <input
+            type="email"
+            id="email"
+            name="email"
+            className="border border-gray-300 rounded px-3 py-2 focus:outline-none focus:ring-2 focus:ring-blue-500"
+          />
+
+          <button
+            type="submit"
+            className="bg-blue-500 text-white rounded px-4 py-2 font-semibold hover:bg-blue-600 focus:outline-none focus:bg-blue-600"
+          >
+            Send
+          </button>
+        </form>
+      </div>
+    </main>
+  );
+}
