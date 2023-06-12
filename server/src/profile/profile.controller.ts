bd855427aa946da7868c72e9fc673e1e0f6671f9
aysllanferreira <aysllan.ferreiraks@gmail.com>
2023-06-12 18:50:48 -0300

    :sparkles: feat: Logic created to fillup data

--- server/src/profile/profile.controller.ts
+++ server/src/profile/profile.controller.ts
@@ -1,4 +1,34 @@
-import { Controller } from '@nestjs/common';
+import {
+  Controller,
+  Post,
+  Req,
+  Res,
+  UseInterceptors,
+  UploadedFile,
+} from '@nestjs/common';
+import { FileInterceptor } from '@nestjs/platform-express';
+import { ProfileService } from './profile.service';
 
 @Controller('profile')
-export class ProfileController {}
+export class ProfileController {
+  constructor(private readonly profileService: ProfileService) {}
+
+  @Post('update')
+  @UseInterceptors(FileInterceptor('picture'))
+  async updateProfile(@UploadedFile() file, @Req() req, @Res() res) {
+    const { first_name, last_name } = req.body;
+    const username = req.user.username;
+
+    try {
+      await this.profileService.updateUserAttributes(
+        username,
+        first_name,
+        last_name,
+        file,
+      );
+      res.status(200).send('Profile updated successfully');
+    } catch (error) {
+      res.status(500).send(error.message);
+    }
+  }
+}
--- server/src/profile/profile.service.ts
+++ server/src/profile/profile.service.ts
@@ -1,4 +1,50 @@
 import { Injectable } from '@nestjs/common';
+import { CognitoIdentityServiceProvider, S3 } from 'aws-sdk';
+import { v4 as uuidv4 } from 'uuid';
 
 @Injectable()
-export class ProfileService {}
+export class ProfileService {
+  private readonly cognitoIdentityServiceProvider: CognitoIdentityServiceProvider;
+  private readonly s3: S3;
+
+  constructor() {
+    this.cognitoIdentityServiceProvider = new CognitoIdentityServiceProvider({
+      region: process.env.AWS_REGION,
+    });
+    this.s3 = new S3({ region: process.env.AWS_REGION });
+  }
+
+  async updateUserAttributes(
+    username: string,
+    firstName: string,
+    lastName: string,
+    pictureFile: any,
+  ) {
+    const userAttributes = [
+      { Name: 'given_name', Value: firstName },
+      { Name: 'family_name', Value: lastName },
+    ];
+
+    if (pictureFile) {
+      const pictureKey = `${username}/${uuidv4()}`;
+      await this.s3
+        .upload({
+          Bucket: process.env.S3_BUCKET_NAME,
+          Key: pictureKey,
+          Body: pictureFile.buffer,
+          ACL: 'public-read', // This allows the file to be read publicly
+        })
+        .promise();
+
+      userAttributes.push({ Name: 'picture', Value: pictureKey });
+    }
+
+    await this.cognitoIdentityServiceProvider
+      .adminUpdateUserAttributes({
+        UserPoolId: process.env.USER_POOL_ID,
+        Username: username,
+        UserAttributes: userAttributes,
+      })
+      .promise();
+  }
+}
