2b88bc230d35bd5a4759c9822649cc4cd74f231e
aysllanferreira <aysllan.ferreiraks@gmail.com>
2023-06-12 18:47:27 -0300

    :sparkles: feat: Profile states created

--- client/src/pages/app/profile.tsx
+++ client/src/pages/app/profile.tsx
@@ -1,7 +1,27 @@
+import { useState } from 'react';
 import me from '@/utils/me';
 
 export default function Profile() {
   me();
+
+  // State to store the form values
+  const [formValues, setFormValues] = useState({ first_name: '', last_name: '', picture: null });
+
+  // Function to handle form changes
+  const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setFormValues({
+      ...formValues,
+      [e.target.name]: e.target.files ? e.target.files[0] : e.target.value,
+    });
+  };
+
+  // Function to handle form submission
+  const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+
+    // TODO: Add the code to make a POST request to the backend
+    console.log(formValues);
+  };
 
   return (
     <main className="flex flex-col items-center justify-center min-h-screen bg-gray-100">
@@ -9,36 +29,45 @@ export default function Profile() {
 
       <p className="text-gray-600 mb-8">Here you can edit your profile.</p>
 
-      <form className="bg-white shadow-md rounded px-8 py-6 w-96">
+      <form className="bg-white shadow-md rounded px-8 py-6 w-96" onSubmit={handleSubmit}>
         <fieldset>
           <legend className="text-lg font-semibold mb-4">Personal information</legend>
 
           <div className="mb-4">
-            <label htmlFor="first_name" className="block text-gray-700 font-medium mb-2">First Name</label>
+            <label htmlFor="first_name" className="block text-gray-700 font-medium mb-2">
+              First Name
+            </label>
             <input
               type="text"
               name="first_name"
               id="first_name"
+              onChange={handleChange}
               className="border border-gray-300 rounded w-full px-3 py-2"
             />
           </div>
 
           <div className="mb-4">
-            <label htmlFor="last_name" className="block text-gray-700 font-medium mb-2">Last Name</label>
+            <label htmlFor="last_name" className="block text-gray-700 font-medium mb-2">
+              Last Name
+            </label>
             <input
               type="text"
               name="last_name"
               id="last_name"
+              onChange={handleChange}
               className="border border-gray-300 rounded w-full px-3 py-2"
             />
           </div>
 
           <div className="mb-4">
-            <label htmlFor="picture" className="block text-gray-700 font-medium mb-2">Picture</label>
+            <label htmlFor="picture" className="block text-gray-700 font-medium mb-2">
+              Picture
+            </label>
             <input
               type="file"
               name="picture"
               id="picture"
+              onChange={handleChange}
               className="border border-gray-300 rounded w-full px-3 py-2"
             />
           </div>
