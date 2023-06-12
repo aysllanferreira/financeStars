96f52354c49cf5b050f24b53337f02d20e96f20a
aysllanferreira <aysllan.ferreiraks@gmail.com>
2023-06-12 17:30:13 -0300

    :sparkles: feat: Slice imported to store

--- client/src/redux/store.ts
+++ client/src/redux/store.ts
@@ -1,7 +1,10 @@
 import { configureStore } from '@reduxjs/toolkit'
+import AuthSlice from './actions/auth'
 
 export const store = configureStore({
-  reducer: {},
+  reducer: {
+    auth: AuthSlice,
+  },
 })
 
 export type RootState = ReturnType<typeof store.getState>
